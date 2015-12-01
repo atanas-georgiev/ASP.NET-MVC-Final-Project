6e7c7fbb5296a7a87d470a84395a00f66388970b
atanas-georgiev <atanasgeorgiev83@gmail.com>
2015-12-01 16:42:57 +0200

    Seed roles in the project

--- src/Planex.Data/Migrations/Configuration.cs
+++ src/Planex.Data/Migrations/Configuration.cs
@@ -1,9 +1,15 @@
 using System.Data.Entity.Migrations;
+using System.Linq;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Planex.Data.Models;
 
 namespace Planex.Data.Migrations
 {
     public sealed class Configuration : DbMigrationsConfiguration<PlanexDbContext>
     {
+        private UserManager<User> userManager;
+
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
@@ -12,7 +18,22 @@ namespace Planex.Data.Migrations
 
         protected override void Seed(PlanexDbContext context)
         {
-            // todo: seed some data
+            if (context.Roles.Any())
+            {
+                return;
+            }
+
+            this.SeedRoles(context);
+        }
+
+        private void SeedRoles(PlanexDbContext context)
+        {
+            context.Roles.AddOrUpdate(x => x.Name, new IdentityRole("Manager"));
+            context.Roles.AddOrUpdate(x => x.Name, new IdentityRole("Load"));
+            context.Roles.AddOrUpdate(x => x.Name, new IdentityRole("Worker"));
+            context.Roles.AddOrUpdate(x => x.Name, new IdentityRole("HR"));
+            context.Roles.AddOrUpdate(x => x.Name, new IdentityRole("StoreKeeper"));
+            context.SaveChanges();
         }
     }
 }
