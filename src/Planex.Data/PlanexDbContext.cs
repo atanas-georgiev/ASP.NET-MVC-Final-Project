d28fb8c92d2bb57d257c600accc48032a4db59c2
atanas-georgiev <atanasgeorgiev83@gmail.com>
2016-02-09 11:44:10 +0200

    Moved user to Planex.Data.Models

---
+++ src/Planex.Data/PlanexDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace Planex.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Planex.Data.Models;
+
+    public class PlanexDbContext : IdentityDbContext<User>
+    {
+        public PlanexDbContext()
+            : base("PlanexDbConnection", false)
+        {
+        }
+
+        public static PlanexDbContext Create()
+        {
+            return new PlanexDbContext();
+        }
+    }
+}
--- src/Planex.Web/Controllers/AccountController.cs
+++ src/Planex.Web/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
+using Planex.Data.Models;
 using Planex.Web.Models;
 
 namespace Planex.Web.Controllers
@@ -151,7 +152,7 @@ namespace Planex.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new User { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
@@ -367,7 +368,7 @@ namespace Planex.Web.Controllers
                 {
                     return View("ExternalLoginFailure");
                 }
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new User { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user);
                 if (result.Succeeded)
                 {
