22adae7ee506682466af2adc8e72bdfd071081e8
atanas-georgiev <atanasgeorgiev83@gmail.com>
2016-02-09 11:34:32 +0200

    Added project skeleton

---
+++ src/Planex.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Planex.Web.Startup))]
+namespace Planex.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
