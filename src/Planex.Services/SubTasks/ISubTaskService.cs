126421b8ce7141b52004d24f40510faa4f2a2779
atanas-georgiev <atanasgeorgiev83@gmail.com>
2016-02-15 09:09:30 +0200

    added sub task service

---
+++ src/Planex.Services/SubTasks/ISubTaskService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Planex.Data.Models;
+
+namespace Planex.Services.SubTasks
+{
+    public interface ISubTaskService
+    {
+        void Add(Subtask task);
+        void Update(Subtask task);
+        void AddAttachments(Subtask dbTask, List<HttpPostedFileBase> uploadedAttachments, HttpServerUtility server);
+        IQueryable<Subtask> GetAll();
+        Subtask GetById(int id);
+    }
+}
---
+++ src/Planex.Services/SubTasks/SubTaskService.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Planex.Common;
+using Planex.Data;
+using Planex.Data.Models;
+
+namespace Planex.Services.SubTasks
+{
+    public class SubTaskService : ISubTaskService
+    {
+        private DbContext context;
+        private IRepository<Subtask> tasks;
+
+        public SubTaskService(DbContext context, IRepository<Subtask> tasks)
+        {
+            this.context = context;
+            this.tasks = tasks;
+        }
+
+        public void Add(Subtask task)
+        {
+            this.tasks.Add(task);
+        }
+
+        public void Update(Subtask task)
+        {
+            this.tasks.Update(task);
+        }
+
+        public void AddAttachments(Subtask dbTask, List<HttpPostedFileBase> uploadedAttachments, HttpServerUtility server)
+        {
+            if (uploadedAttachments == null)
+            {
+                return;
+            }
+
+            if (!Directory.Exists(server.MapPath(TasksConstants.MainContentFolder)))
+            {
+                Directory.CreateDirectory(server.MapPath(TasksConstants.MainContentFolder));
+            }
+
+            if (!Directory.Exists(server.MapPath(TasksConstants.MainContentFolder + "\\" + dbTask.Id)))
+            {
+                Directory.CreateDirectory(server.MapPath(TasksConstants.MainContentFolder + "\\" + dbTask.Id));
+            }
+
+            foreach (var file in uploadedAttachments)
+            {
+                var filename = Path.GetFileName(file.FileName);
+                file.SaveAs(server.MapPath(TasksConstants.MainContentFolder + "\\" + dbTask.Id + "\\" + filename));
+                dbTask.Attachments.Add(new Attachment()
+                {
+                    Name = file.FileName
+                });
+
+                Update(dbTask);
+            }
+        }
+
+        public IQueryable<Subtask> GetAll()
+        {
+            return this.tasks.All();
+        }
+
+        public Subtask GetById(int id)
+        {
+            return this.tasks.GetById(id);
+        }
+    }
+}
