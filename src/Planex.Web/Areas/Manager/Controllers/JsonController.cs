3bfb6e85fb96fbc92fe3402c93195cc5bc7c3b01
atanas-georgiev <atanasgeorgiev83@gmail.com>
2016-02-16 09:54:50 +0200

    Added manager project page and model

---
+++ src/Planex.Web/Areas/Manager/Controllers/JsonController.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Planex.Services.Skills;
+using Planex.Services.SubTasks;
+using Planex.Services.Tasks;
+using Planex.Services.Users;
+using Planex.Web.Areas.Manager.Models;
+using Planex.Web.Infrastructure.Mappings;
+
+namespace Planex.Web.Areas.Manager.Controllers
+{
+    public class JsonController : BaseController
+    {
+        // GET: Manager/Json
+        private readonly IUserService userService;
+        private readonly ISkillService skillService;
+        private readonly ISubTaskService subTaskService;
+        private readonly ITaskService taskService;
+
+        public JsonController(IUserService userService,
+                                ISkillService skillService,
+                                ISubTaskService subTaskService,
+                                ITaskService taskService
+                                ) : base(userService)
+        {
+            this.skillService = skillService;
+            this.subTaskService = subTaskService;
+            this.userService = userService;
+            this.taskService = taskService;
+        }
+
+        public ActionResult Projects_Read([DataSourceRequest]DataSourceRequest request)
+        {
+            var tasks = taskService.GetAll().To<ProjectViewModel>();
+            return Json(tasks.ToDataSourceResult(request));
+        }
+    }
+}
---
+++ src/Planex.Web/Areas/Manager/Models/ProjectViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using AutoMapper;
+using Planex.Data.Models;
+using Planex.Web.Infrastructure.Mappings;
+
+namespace Planex.Web.Areas.Manager.Models
+{
+    public class ProjectViewModel : IMapTo<MainTask>, IHaveCustomMappings
+    {
+        [Key]
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(50)]
+        [UIHint("String")]
+        public string Title { get; set; }
+
+        [Required]
+        [UIHint("EnumDropDown")]
+        public PriorityType Priority { get; set; }
+
+        [Required]
+        [UIHint("String")]
+        public string Manager { get; set; }
+
+        [Required]
+        [UIHint("String")]
+        public string Lead { get; set; }
+
+        [Required]
+        [UIHint("Number")]
+        public int Completed { get; set; }
+
+        [Required]
+        public TaskStateType State { get; set; }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            configuration.CreateMap<MainTask, ProjectViewModel>("")
+                .ForMember(m => m.Manager, opt => opt.MapFrom(c => c.Manager.FirstName + " " + c.Manager.LastName))
+                .ForMember(m => m.Lead, opt => opt.MapFrom(c => c.Lead.FirstName + " " + c.Lead.LastName))
+                .ForMember(m => m.Completed, opt => opt.MapFrom(c => 0));
+        }
+    }
+}
